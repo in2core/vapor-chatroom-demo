7ec9f185e3853def4fec3d491effa085b72562c7
Filip Klembara <filip@klembara.pro>
2022-05-05 14:41:51 +0200

    Remove .env

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,9 +15,9 @@ public func configure(_ app: Application) throws {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "chatroom"
     ), as: .psql)
 
     app.migrations.add(CreateMessage())
